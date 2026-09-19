771c3352932b5227896c23c78064f6ff180089a8
bailaoshi <449708913@qq.com>
2026-09-19 23:22:28 +0800

    add windows maker and bat file

---
+++ windows/pool.bat
@@ -0,0 +1,3 @@
+:start
+cpuminer-sse2.exe -a yespower -o stratum+tcp://IP or domain:port -u sugar1q674kme4mr8jfajsmau57y5aaqmmzde5kdtusf5.001 -t1
+goto start
---
+++ windows/solo.bat
@@ -0,0 +1,3 @@
+:start
+cpuminer-sse2.exe -a yespower -o http://127.0.0.1:34229 --no-longpoll -u baihe -p passwordbaihe --coinbase-addr=sugar1q674kme4mr8jfajsmau57y5aaqmmzde5kdtusf5 -t2
+goto start
